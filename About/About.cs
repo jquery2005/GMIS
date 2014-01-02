cc2506e91fdb317e079d18453db570638c5e4c3e
jquery2005 <jquery2005@gmail.com>
2014-01-02 19:11:16 +0800

    AddAboutLibrary
    添加“帮助/关于“

---
+++ About/About.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Win32;
+
+namespace About
+{
+    public partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void About_Load(object sender, EventArgs e)
+        {
+            System.Collections.IDictionary ii;
+            ii = Environment.GetEnvironmentVariables();
+            string[] ss;
+            ss = Environment.GetLogicalDrives();
+
+            label4.Text = ReadRememberUser();
+            label5.Text = ReadRememberField();
+
+            GMIS.CommonRightBusinessRule.CCommonInfo gCCommonInfo = new GMIS.CommonRightBusinessRule.CCommonInfo();
+            string ss1, struser = ".", strserver = "gmisinfo";
+            try
+            {
+                ss1 = gCCommonInfo.gsGetConfigPropertyValue("发布日期", "日期");
+            }
+            catch
+            {
+                ss1 = "";
+            }
+            label8.Text = ss1;
+
+            try
+            {
+                if (System.IO.File.Exists(Application.StartupPath + @"\gmisinfo.txt"))
+                {
+                    System.IO.StreamReader sr = System.IO.File.OpenText(Application.StartupPath + @"\gmisinfo.txt");
+                    string str = sr.ReadLine();
+                    if (str == null) str = "";
+                    if (str.Trim() != "" && str.Split(',').Length == 3)
+                    {
+                        struser = str.Split(',')[0];
+                        strserver = str.Split(',')[1];
+                    }
+                    else
+                    {
+                        struser = ".";
+                        strserver = "gmisinfo";
+                    }
+                    sr.Close();
+                }
+                else  //不存在文件
+                {
+                    struser = ".";
+                    strserver = "gmisinfo";
+                }
+            }
+            catch
+            {
+                struser = ".";
+                strserver = "gmisinfo";
+            }
+            try
+            {
+                DataTable dt = new DataTable();
+                string dbConnectStr = "server=" + struser + ";uid=gmis2002"
+                    + ";pwd=gs2002;database=" + strserver + ";Connection Lifetime = 180";
+                SqlConnection conn = new SqlConnection(dbConnectStr);
+                SqlDataAdapter dsCommand = new SqlDataAdapter("select convert(varchar(10),verDate,21) from gs_ver ", conn);
+                dsCommand.Fill(dt);
+                ss1 = dt.Rows[0][0].ToString();
+            }
+            catch
+            {
+                ss1 = "";
+            }
+
+            label10.Text = ss1;
+
+            this.TopMost = true;
+        }
+
+        /// <summary>
+        /// 从注册表中读字符记忆型数据
+        /// </summary>
+        private string ReadRememberField()
+        {
+            object strRemember;
+            try
+            {
+                RegistryKey LMRK = Registry.LocalMachine;
+                RegistryKey SoftRK = LMRK.OpenSubKey("software", true);
+                RegistryKey GmisRK = SoftRK.OpenSubKey("Microsoft").OpenSubKey("Windows NT").OpenSubKey("CurrentVersion");
+
+                strRemember = GmisRK.GetValue("RegisteredOrganization");
+                if (strRemember == null || strRemember.ToString() == "")
+                {
+                    GmisRK = SoftRK.OpenSubKey("Microsoft").OpenSubKey("Windows").OpenSubKey("CurrentVersion");
+
+                    strRemember = GmisRK.GetValue("RegisteredOrganization");
+                }
+
+                return strRemember.ToString();
+            }//try
+            catch
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// 从注册表中读字符记忆型数据
+        /// </summary>
+        private string ReadRememberUser()
+        {
+            object strRemember;
+            try
+            {
+                RegistryKey LMRK = Registry.LocalMachine;
+                RegistryKey SoftRK = LMRK.OpenSubKey("software", true);
+                RegistryKey GmisRK = SoftRK.OpenSubKey("Microsoft").OpenSubKey("Windows NT").OpenSubKey("CurrentVersion");
+
+                strRemember = GmisRK.GetValue("RegisteredOwner");
+                if (strRemember == null || strRemember.ToString() == "")
+                {
+                    GmisRK = SoftRK.OpenSubKey("Microsoft").OpenSubKey("Windows").OpenSubKey("CurrentVersion");
+
+                    strRemember = GmisRK.GetValue("RegisteredOwner");
+                }
+                return strRemember.ToString();
+            }//try
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ About/About.designer.cs
@@ -0,0 +1,197 @@
+﻿namespace About
+{
+    partial class About
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(320, 131);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(88, 16);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "label10";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(192, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(104, 16);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "数据库发布日期：";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(320, 111);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 16);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "label8";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(192, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 16);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "程序发布日期：";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(242)))), ((int)(((byte)(245)))));
+            this.label6.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label6.Location = new System.Drawing.Point(8, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 152);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "警告：             本计算机程序受版权法和国际条例保护。如未经授权而擅自复制或传播本程序（或其中任何一部分），将受到严厉的民事和刑事制裁，并将在法律许" +
+    "可的最大限度内受到起诉。";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(192, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 16);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "label5";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(192, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 16);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "本产品使用权属于：";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(336, 203);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(72, 24);
+            this.btnOk.TabIndex = 15;
+            this.btnOk.Text = "确定";
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(192, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "版本号：1.0.0.20131230";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(192, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 12);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "全国军队转业干部安置信息管理系统";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(418, 64);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(192, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "label4";
+            // 
+            // About
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(418, 231);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "About";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "关于";
+            this.Load += new System.EventHandler(this.About_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
